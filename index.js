98617dd4b295e7064d6becc188fb6aec16f5bba6
Tahir J <icodejs@gmail.com>
2016-02-15 15:33:00 +0000

    Make port dynamic

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 1337;
+const port = process.env.PORT || 5000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+}).listen(port);
