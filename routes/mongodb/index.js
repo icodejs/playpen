e91b1bb8460a9f903cceca3578b67c3ed040a6bf
Tahir J <icodejs@gmail.com>
2016-02-19 10:12:13 +0000

    Airbnb linting

--- routes/mongodb/index.js
+++ routes/mongodb/index.js
@@ -1,10 +1,11 @@
+/* eslint new-cap: 0*/
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('../../lib/mongodb/models/users');
+const Users = require('../../lib/mongodb/models/users');
 const router = express.Router();
 
 router.get('/user/save', (req, res) => {
-    const user = new users({ name: 'bill' });
+    const user = new Users({ name: 'bill' });
 
     user.save(err => {
         if (err) return res.status(500).send(err.stack);
